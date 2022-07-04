f55a9f6322332a9aaade9883fd967e7d35855018
Felix Marty <felix@huggingface.co>
2022-07-04 11:08:33 +0200

    debig

---
+++ test_git.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+import tempfile
+
+if __name__ == "__main__":
+    dir_path = tempfile.mkdtemp("evaluator_trainer_parity_test")
+
+    print("dir_path", dir_path)
+
+    transformers_version = "4.20.1"
+    branch = ""
+    if not transformers_version.endswith(".dev0"):
+        branch = f"--branch v{transformers_version}"
+    subprocess.run(
+        f"git clone --depth 3 --filter=blob:none --sparse {branch} https://github.com/huggingface/transformers",
+        shell=True,
+        cwd=dir_path,
+    )
+
+    print("after git clone", dir_path)
+
+
+    print("self.dir_path", dir_path)
+
+    subprocess.run(
+        "git sparse-checkout set examples/pytorch/text-classification",
+        shell=True,
+        cwd=os.path.join(dir_path, "transformers"),
+    )
+
+    print("after sparse-checkout", os.path.join(dir_path, "transformers"))
